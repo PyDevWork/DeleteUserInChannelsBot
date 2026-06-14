8bcb85b452ad7f52a3e0321c20e80e7cf56ec0a1
Nazar <nazar.fedorowych@gmail.com>
2026-06-14 13:50:47 +0300

    update

--- src/bot/utils/admin.py
+++ src/bot/utils/admin.py
@@ -1,4 +1,5 @@
 from typing import List
+
 from src.database.core import Database
 
 
@@ -6,4 +7,6 @@ async def is_admin(user_id: int, admins: List, db: Database) -> bool:
     if user_id in admins:
         return True
     user_info = await db.user.select(user_id=user_id)
+    if user_info is None:
+        return False
     return user_info.admin
